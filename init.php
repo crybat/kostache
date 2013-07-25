5656b3d57ced61b22b1c639da7b2026219268238
Hinton <hinton.oscar@gmail.com>
2013-07-25 19:19:27 +0200

    Removed mustache submodule. Renamed package name to modulargaming/KOstache.

--- init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-// Load Mustache for PHP
-if ( ! class_exists('Mustache_Engine')) {
-	include Kohana::find_file('vendor', 'mustache/src/Mustache/Autoloader');
-	Mustache_Autoloader::register();
-}
